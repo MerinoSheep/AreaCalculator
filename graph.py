ccf0a09e944a0a67413b1cf2c57807ec9feb5791
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-03-08 23:51:15 -0500

    function
    graph.py is run from main.py

--- graph.py
+++ graph.py
@@ -2,52 +2,58 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy
 import math
-DRAWTYPE = False # true means rectangle false means trapezoid
-RSUMTYPE = "LEFT" #MIDDLE,LEFT,OR RIGHT
-xValEquation = []
-yValEquation = []
-xValSum = []
-yValSum = []  # The y values for the riemman sum will be stored here
-STARTRANGE = 0
-ENDRANGE = 3.14
-N = 5
-def equation(i):
-	return math.cos(2*i)
 
-fig, ax = plt.subplots()
-currentAxis = plt.gca()
-deltaX = (ENDRANGE-.1)-STARTRANGE
 
-for i in numpy.arange(STARTRANGE, ENDRANGE, .1): # Two arrays are stored, one for x values of the equation and another for the y values
-    xValEquation.append(i)
-    yValEquation.append(equation(i))
+def draw(STARTRANGE,ENDRANGE,N):
+    ENDRANGE += .1
+    xValEquation = []
+    yValEquation = []
+    xValSum = []
+    yValSum = []
+    
+    RSUMTYPE = "LEFT" #MIDDLE,LEFT,OR RIGHT
+    # The y values for the riemman sum will be stored here
+    
+    def equation(i):
+        return math.cos(i)
 
-if(RSUMTYPE == "LEFT" and DRAWTYPE ):
-    for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):  # LEFT RIEMANN SUMS
-        currentAxis.add_patch(patches.Rectangle(
-            (i, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
-        #print(i)
-elif(RSUMTYPE == "RIGHT"):
-    for i in numpy.arange(ENDRANGE-.1,STARTRANGE,-(deltaX/N)):  # RIGHT RIEMANN SUMS
-        currentAxis.add_patch(patches.Rectangle(
-            (i-(deltaX/N), 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
-        #print(i)
+    fig, ax = plt.subplots()
+    currentAxis = plt.gca()
+    deltaX = (ENDRANGE-.1)-STARTRANGE
 
-elif(RSUMTYPE == "MIDDLE"):
-	for i in numpy.arange(STARTRANGE+(deltaX/N)/2, ENDRANGE-.1,(deltaX/N)):  # MIDDLE RIEMANN SUMS
-		currentAxis.add_patch(patches.Rectangle(
-            (i-(deltaX/N)/2, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
-		print(i)
+    for i in numpy.arange(STARTRANGE, ENDRANGE, .1): # Two arrays are stored, one for x values of the equation and another for the y values
+        xValEquation.append(i)
+        yValEquation.append(equation(i))
 
-if(not DRAWTYPE): ## TRAPEZOID
+    if(RSUMTYPE == "LEFT" and DRAWTYPE ):
+        for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):  # LEFT RIEMANN SUMS
+            currentAxis.add_patch(patches.Rectangle(
+                (i, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
+            #print(i)
+    elif(RSUMTYPE == "RIGHT"):
+        for i in numpy.arange(ENDRANGE-.1,STARTRANGE,-(deltaX/N)):  # RIGHT RIEMANN SUMS
+            currentAxis.add_patch(patches.Rectangle(
+                (i-(deltaX/N), 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
+            #print(i)
 
-	for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):
-		x=[i,i+deltaX/N,i+deltaX/N,i,] #BL,BR,TR, TL
-		y=[0,0,equation(i+deltaX/N),equation(i)]
-		currentAxis.add_patch(patches.Polygon(xy=list(zip(x,y)),fill= False, linewidth=1, edgecolor='r', facecolor='none'))
-# print(xValEquation)
-plt.plot(xValEquation, yValEquation)
-plt.plot(xValSum, yValSum, 'r')
-plt.ylabel('y values')
-plt.xlabel('x values')
-plt.show()
+    elif(RSUMTYPE == "MIDDLE"):
+        for i in numpy.arange(STARTRANGE+(deltaX/N)/2, ENDRANGE-.1,(deltaX/N)):  # MIDDLE RIEMANN SUMS
+            currentAxis.add_patch(patches.Rectangle(
+                (i-(deltaX/N)/2, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
+            print(i)
+
+    if(not DRAWTYPE): ## TRAPEZOID
+
+        for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):
+            x=[i,i+deltaX/N,i+deltaX/N,i,] #BL,BR,TR, TL
+            y=[0,0,equation(i+deltaX/N),equation(i)]
+            currentAxis.add_patch(patches.Polygon(xy=list(zip(x,y)),fill= False, linewidth=1, edgecolor='r', facecolor='none'))
+    # print(xValEquation)
+    plt.plot(xValEquation, yValEquation)
+    plt.plot(xValSum, yValSum, 'r')
+    plt.ylabel('y values')
+    plt.xlabel('x values')
+    plt.show()
+
+if __name__ == "__main__":
+    draw()
