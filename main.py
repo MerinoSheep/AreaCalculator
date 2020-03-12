4ee105313c3aa7a2d65432069161f5adc77a776f
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-03-12 09:00:59 -0500

    documentation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ STARTRANGE = 0
 ENDRANGE = 15
 N = 20
 DRAWTYPE = None # true means rectangle false means trapezoid
-def runGraph(STARTRANGE,ENDRANGE,N,DRAWTYPE):
+def runGraph(ENDRANGE,N,DRAWTYPE):
     status, STARTRANGE =  retrieve_n()
     if status:
         graph.draw(STARTRANGE,ENDRANGE,N,DRAWTYPE)
@@ -40,25 +40,26 @@ def retrieve_n():
         return False, 0
     return True, strtRange
 
-
+#Text Labels Next to Input Fields
 fxText = Label(window, text = "Function")
 startRangeText = Label(window, text = "Start Range")
 endRangeText = Label(window, text = "End Range")
 nText = Label(window, text = "N")   
-
+#Initialize Labels to grid
 fxText.grid(row = 0,column = 0)
 startRangeText.grid(row = 1,column = 0)
 endRangeText.grid(row = 2,column = 0)
 nText.grid(row =3,column = 0)
-
-graphButton = tkinter.Button(window, text = "Graph", command = lambda: runGraph(STARTRANGE,ENDRANGE,N,DRAWTYPE))
+#Graph Button
+graphButton = tkinter.Button(window, text = "Graph", command = lambda: runGraph(ENDRANGE,N,DRAWTYPE))
+#Initialize Buttons
 graphButton.grid(row = 4, column = 1, pady = 2)
-
+#Text Entry Fields
 startRangeEntry = Entry(window)
 endRangeEntry = Entry(window)
 nEntry = Entry(window)
 fxEntry = Entry(window)
-
+#Initialize Text Entry Fields to 
 fxEntry.grid(row=0, column = 1,sticky = W,pady = 2)
 startRangeEntry.grid(row=1, column = 1,sticky = W,pady = 2)
 endRangeEntry.grid(row=2,column = 1,stick = W,pady = 2)
