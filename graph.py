ee862f3217606f24f2b50a066ad3650644e2bd6f
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-03-19 18:37:08 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,55 +1,57 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import numpy
+import numpy as np
 from math import *
 
 
 def draw(STARTRANGE,ENDRANGE,N,DRAWTYPE,fx,RSUMTYPE):
     ENDRANGE += .1
-    xValEquation = []
+    x_vals = np.arange(float(STARTRANGE),ENDRANGE,.1) ## change floats later
     yValEquation = []
-    xValSum = []
-    yValSum = []
-  #MIDDLE,LEFT,OR RIGHT
-    # The y values for the riemman sum will be stored here
+    xValSum = [] ## Rectangles or Trapezoids
+    yValSum = []  ## ^
     
     def equation(i):
         x=i
         return eval(fx)
+
     fig, ax = plt.subplots()
     currentAxis = plt.gca()
     deltaX = (ENDRANGE-.1)-STARTRANGE
 
-    for i in numpy.arange(STARTRANGE, ENDRANGE, .1): # Two arrays are stored, one for x values of the equation and another for the y values
-        x=i
-        xValEquation.append(i)
-        yValEquation.append(eval(fx)) 
 
+    for x in x_vals: # Two arrays are stored, one for x values of the equation and another for the y values
+
+    
+        yValEquation.append(eval(fx))
+
+
+    #Draws Rectangles
     if(RSUMTYPE == "LEFT" and DRAWTYPE ):
-        for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):  # LEFT RIEMANN SUMS
+        for i in np.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):  # LEFT RIEMANN SUMS
             currentAxis.add_patch(patches.Rectangle(
                 (i, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
             #print(i)
     elif(RSUMTYPE == "RIGHT" and DRAWTYPE):
-        for i in numpy.arange(ENDRANGE-.1,STARTRANGE,-(deltaX/N)):  # RIGHT RIEMANN SUMS
+        for i in np.arange(ENDRANGE-.1,STARTRANGE,-(deltaX/N)):  # RIGHT RIEMANN SUMS
             currentAxis.add_patch(patches.Rectangle(
                 (i-(deltaX/N), 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
             #print(i)
 
     elif(RSUMTYPE == "MIDDLE"and DRAWTYPE):
-        for i in numpy.arange(STARTRANGE+(deltaX/N)/2, ENDRANGE-.1,(deltaX/N)):  # MIDDLE RIEMANN SUMS
+        for i in np.arange(STARTRANGE+(deltaX/N)/2, ENDRANGE-.1,(deltaX/N)):  # MIDDLE RIEMANN SUMS
             currentAxis.add_patch(patches.Rectangle(
                 (i-(deltaX/N)/2, 0), (deltaX/N), equation(i), linewidth=1, edgecolor='r', facecolor='none'))
             print(i)
 
     if(not DRAWTYPE): ## TRAPEZOID
 
-        for i in numpy.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):
+        for i in np.arange(STARTRANGE, ENDRANGE-.1, (deltaX/N)):
             x=[i,i+deltaX/N,i+deltaX/N,i,] #BL,BR,TR, TL
             y=[0,0,equation(i+deltaX/N),equation(i)]
             currentAxis.add_patch(patches.Polygon(xy=list(zip(x,y)),fill= False, linewidth=1, edgecolor='r', facecolor='none'))
-    # print(xValEquation)
-    plt.plot(xValEquation, yValEquation)
+    # print(x_vals)
+    plt.plot(x_vals, yValEquation)
     plt.plot(xValSum, yValSum, 'r')
     plt.ylabel('y values')
     plt.xlabel('x values')
