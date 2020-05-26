0c87828cb2e3de07958d9ce6ba5949d835be2f75
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-05-26 15:07:09 -0500

    OOP
    Main window is now a object. Allows other modules to modify text vars and other properties

--- graph.py
+++ graph.py
@@ -1,22 +1,23 @@
 '''graph.py'''
 
-from math import * # pylint: disable=unused-wildcard-import disable=wildcard-import
-from math import log as ln # pylint: disable=unused-import
-from math import log10 as log # pylint: disable=unused-import
+from math import *  # pylint: disable=unused-wildcard-import disable=wildcard-import
+from math import log as ln  # pylint: disable=unused-import
+from math import log10 as log  # pylint: disable=unused-import
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
+
 
 def draw(start_range, end_range, N, drawtype, fx, RSUMTYPE):
     """Starts graphing equation with sum"""
     end_range += .1
     rect_linewidth = 1
-    x_vals = np.arange(float(start_range), end_range, .1)
+    x_vals = np.arange(float(start_range), end_range, .05)
     y_val_equation = []
     x_val_sum = []
     y_val_sum = []
 
-    def equation(x):
+    def equation(x): # pylint: disable=unused-argument
         return eval(fx)
 
     fig, ax = plt.subplots()
@@ -30,33 +31,33 @@ def draw(start_range, end_range, N, drawtype, fx, RSUMTYPE):
             return False  # Allows main to show error
 
     # Draws Rectangles
-    if(RSUMTYPE == "Left" and drawtype):
-        for i in np.arange(start_range, end_range-.1, (rectangle_length)):  # LEFT RIEMANN SUMS
-            current_axis.add_patch(patches.Rectangle(
-                (i, 0), (rectangle_length), equation(i),
-                linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
+    if drawtype is not None:
+        if drawtype:
+            if RSUMTYPE == "Left":  # LEFT RIEMANN SUMS
+                for i in np.arange(start_range, end_range-.1, rectangle_length):
+                    current_axis.add_patch(patches.Rectangle(
+                        (i, 0), (rectangle_length), equation(i),
+                        linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
-    elif(RSUMTYPE == "Right" and drawtype):
-        for i in np.arange(end_range-.1, start_range, -(rectangle_length)):  # RIGHT RIEMANN SUMS
-            current_axis.add_patch(patches.Rectangle(
-                (i-(rectangle_length), 0), (rectangle_length), equation(i),
-                linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
+            elif RSUMTYPE == "Right":  # RIGHT RIEMANN SUMS
+                for i in np.arange(end_range-.1, start_range, -(rectangle_length)):
+                    current_axis.add_patch(patches.Rectangle(
+                        (i-(rectangle_length), 0), (rectangle_length), equation(i),
+                        linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
-    elif(RSUMTYPE == "Middle" and drawtype):
-        # MIDDLE RIEMANN SUMS
-        for i in np.arange(start_range+(rectangle_length)/2, end_range-.1, (rectangle_length)):
-            current_axis.add_patch(patches.Rectangle(
-                (i-(rectangle_length)/2, 0), (rectangle_length), equation(i),
-                linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
+            elif RSUMTYPE == "Middle":  # MIDDLE RIEMANN SUMS
+                for i in np.arange(start_range+rectangle_length/2, end_range-.1, rectangle_length):
+                    current_axis.add_patch(patches.Rectangle(
+                        (i-(rectangle_length)/2, 0), (rectangle_length), equation(i),
+                        linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
-    if not drawtype and drawtype != None:  # TRAPEZOID
-
-        for i in np.arange(start_range, end_range-.1, (rectangle_length)):
-            # BL,BR,TR, TL order of how nump draws the polygons
-            x = [i, i+rectangle_length, i+rectangle_length, i, ]
-            y = [0, 0, equation(i+rectangle_length), equation(i)]
-            current_axis.add_patch(patches.Polygon(xy=list(zip(
-                x, y)), fill=False, linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
+        elif not drawtype:  # TRAPEZOID
+            for i in np.arange(start_range, end_range-.1, (rectangle_length)):
+                # BL,BR,TR, TL order of how nump draws the polygons
+                x = [i, i+rectangle_length, i+rectangle_length, i, ]
+                y = [0, 0, equation(i+rectangle_length), equation(i)]
+                current_axis.add_patch(patches.Polygon(xy=list(zip(
+                    x, y)), fill=False, linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
     ax.grid(linewidth='.25')
     ax.set_axisbelow(True)
--- main.py
+++ main.py
@@ -1,79 +1,147 @@
 '''main.py'''
-from tkinter import Tk, ttk, StringVar, Label, Button, Entry, W, EW, Checkbutton, HORIZONTAL
+# TODO limit eval
+from tkinter import Checkbutton, Tk, ttk, StringVar, Label, Button, Entry, W, EW,  HORIZONTAL, Toplevel, Frame
 import graph
-
-window = Tk()
-window.title("Area Visualization")
-
+import integral_window
 
 
 DRAWTYPE = None
 
+class MainWindow:
+    def __init__(self, master):
+        #Frame.__init__(self, master)
+        self.master = master
+        self.error_var = StringVar()
+        # Labels
+        self.fxText = Label(self.master, text="Function")
+        self.startRangeText = Label(self.master, text="Start Range")
+        self.endRangeText = Label(self.master, text="End Range")
+        self.nText = Label(self.master, text="N")
+        self.error_text = Label(
+            self.master, fg="red", textvariable=self.error_var, font='bold')
 
-def run_graph():
-    '''Grab input from tk.entrys'''
-    status_s, start_range = retrieve_strt()  # statuses are a boolean return
-    status_e, end_range = retrieve_end()
-    status_n, n = retrieve_n()
-    fx = retrieve_fx()
-    if status_s and status_e and status_n:
-        if end_range > start_range:
-            error_var.set('')
-            if not graph.draw(start_range, end_range, n, DRAWTYPE, fx, menu.get()):
-                error_var.set('Invalid Function')
-        else:
-            error_var.set('Invalid Function')
+        # Initialize Labels to grid
+        self.fxText.grid(row=0, column=0)
+        self.startRangeText.grid(row=1, column=0)
+        self.endRangeText.grid(row=2, column=0)
+        self.nText.grid(row=3, column=0)
+        self.error_text.grid(row=4, column=3)
+
+        # Button
+        self.graphButton = Button(
+            self.master, text="Graph", command=self.run_graph)
+        self.graphButton.grid(row=4, column=1, pady=2)
+        self.calculate_button = Button(
+            self.master, text="Calculate", command= self.run_calculate)
+        self.calculate_button.grid(row=4, column=0, pady=2)
+        # Text Entry Fields
+        self.startRangeEntry = Entry(self.master, validate="key")
+        self.endRangeEntry = Entry(self.master)
+        self.nEntry = Entry(self.master, validate="key")
+        self.nEntry['validatecommand'] = (
+            self.nEntry.register(test_val), '%P', '%d')
+        self.fxEntry = Entry(self.master)
+
+        # Initialized to Grid
+        self.fxEntry.grid(row=0, column=1, sticky=W, pady=2)
+        self.startRangeEntry.grid(row=1, column=1, sticky=W, pady=2)
+        self.endRangeEntry.grid(row=2, column=1, stick=W, pady=2)
+        self.nEntry.grid(row=3, column=1, stick=W, pady=2)
+
+        self.options = ["LEFT", "RIGHT", "MIDDLE"]
+        self.splash = StringVar(self.master)
+        self.splash.set("Riemman Draw Location")
+        # Combobox
+        self.menu = ttk.Combobox(self.master, textvariable=self.splash, values=[
+            "Left", "Right", "Middle"], state="readonly")
+        self.menu.grid(row=3, column=3, sticky=EW, pady=2,)
+
+        # CheckButton
+        self.is_riemann_check = Checkbutton(
+            self.master, text="Riemann", command=self.is_riemann_check)
+        self.is_trapezoid_check = Checkbutton(
+            self.master, text="Trapezoid", command=self.is_trapezoid_check)
+        # Initialized
+        self.is_riemann_check.grid(row=0, column=3, sticky=W, pady=2)
+        self.is_trapezoid_check.grid(row=1, column=3, sticky=W, pady=2)
+
+        # Breakline
+        self.break_line = ttk.Separator(orient=HORIZONTAL)
+        self.break_line.grid(row=2, column=3, sticky=EW, pady=2)
 
 
-def is_riemann_check():
-    '''Reverses the menu disabled from is_trapezoid_check()'''
-    global DRAWTYPE
-    DRAWTYPE = True
-    menu.state(['!disabled'])
-    is_trapezoid_check.deselect()
+    def run_graph(self):
+        '''Grab input from tk.entrys'''
+        status_s, start_range = self.retrieve_strt()  # statuses are a boolean return
+        status_e, end_range = self.retrieve_end()
+        status_n, n = self.retrieve_n()
+        fx = self.retrieve_fx()
+        if status_s and status_e and status_n:
+            if end_range > start_range:
+                self.error_var.set('')
+                graph.draw(start_range, end_range, n,
+                        DRAWTYPE, fx, self.menu.get())
+                #error_var.set('Invalid Function')
+            else:
+                self.error_var.set('Invalid Function')
 
 
-def is_trapezoid_check():
-    ''' disables the Riemann sum menu because Trapezoids only have one state'''
-    global DRAWTYPE
-    DRAWTYPE = False
-    menu.state(['disabled'])
-    is_riemann_check.deselect()
+    def run_calculate(self):
+        '''Runs integral root'''
+        #t = Toplevel()
+        #t.title("Hello")
+        integral = integral_window.IntegralWindow(
+            0, 5, 4, '2*x')
+
+    def is_riemann_check(self):
+        '''Reverses the menu disabled from is_trapezoid_check()'''
+        global DRAWTYPE
+        DRAWTYPE = True
+        self.menu.state(['!disabled'])
+        self.is_trapezoid_check.deselect()
 
 
-def retrieve_strt():
-    '''Does some simple checks before passing strtRange to draw(*args)'''
-    try:
-        strt_range = float(startRangeEntry.get())
-    except ValueError:
-        error_var.set('Input a valid starting range')
-        return False, 0
-    return True, strt_range
+    def is_trapezoid_check(self):
+        ''' disables the Riemann sum menu because Trapezoids only have one state'''
+        global DRAWTYPE
+        DRAWTYPE = False
+        self.menu.state(['disabled'])
+        self.is_riemann_check.deselect()
 
 
-def retrieve_end():
-    '''Retrieves endrange of function'''
-    try:
-        end_range = float(endRangeEntry.get())
-    except ValueError:
-        error_var.set('Input a valid end range')
-        return False, 0
-    return True, end_range
+    def retrieve_strt(self):
+        '''Does some simple checks before passing strtRange to draw(*args)'''
+        try:
+            strt_range = float(self.startRangeEntry.get())
+        except ValueError:
+            self.error_var.set('Input a valid starting range')
+            return False, 0
+        return True, strt_range
 
 
-def retrieve_n():
-    '''Retrieves number of rectangles wanted'''
-    try:
-        n = float(nEntry.get())
-    except ValueError:
-        return False, 0
-    return True, n
+    def retrieve_end(self):
+        '''Retrieves endrange of function'''
+        try:
+            end_range = float(self.endRangeEntry.get())
+        except ValueError:
+            self.error_var.set('Input a valid end range')
+            return False, 0
+        return True, end_range
 
 
-def retrieve_fx():
-    '''retrieve function'''
-    fx = fxEntry.get()
-    return fx.lower()
+    def retrieve_n(self):
+        '''Retrieves number of rectangles wanted'''
+        try:
+            n = float(self.nEntry.get())
+        except ValueError:
+            return False, 0
+        return True, n
+
+
+    def retrieve_fx(self):
+        '''retrieve function'''
+        fx = self.fxEntry.get()
+        return fx.lower()
 
 
 def test_val(in_str, acttyp):
@@ -84,57 +152,7 @@ def test_val(in_str, acttyp):
     return True
 
 
-error_var = StringVar()
-# Labels
-fxText = Label(window, text="Function")
-startRangeText = Label(window, text="Start Range")
-endRangeText = Label(window, text="End Range")
-nText = Label(window, text="N")
-error_text = Label(window, fg="red", textvariable=error_var, font='bold')
-
-# Initialize Labels to grid
-fxText.grid(row=0, column=0)
-startRangeText.grid(row=1, column=0)
-endRangeText.grid(row=2, column=0)
-nText.grid(row=3, column=0)
-error_text.grid(row=4, column=3)
-
-# Button
-graphButton = Button(window, text="Graph", command=run_graph)
-graphButton.grid(row=4, column=1, pady=2)
-
-# Text Entry Fields
-startRangeEntry = Entry(window, validate="key")
-endRangeEntry = Entry(window)
-nEntry = Entry(window, validate="key")
-nEntry['validatecommand'] = (nEntry.register(test_val), '%P', '%d')
-fxEntry = Entry(window)
-
-# Initialized to Grid
-fxEntry.grid(row=0, column=1, sticky=W, pady=2)
-startRangeEntry.grid(row=1, column=1, sticky=W, pady=2)
-endRangeEntry.grid(row=2, column=1, stick=W, pady=2)
-nEntry.grid(row=3, column=1, stick=W, pady=2)
-
-options = ["LEFT", "RIGHT", "MIDDLE"]
-splash = StringVar(window)
-splash.set("Riemman Draw Location")
-# Combobox
-menu = ttk.Combobox(window, textvariable=splash, values=[
-    "Left", "Right", "Middle"], state="readonly")
-menu.grid(row=3, column=3, sticky=EW, pady=2,)
-
-# CheckButton
-is_riemann_check = Checkbutton(
-    window, text="Riemann", command=is_riemann_check)
-is_trapezoid_check = Checkbutton(
-    window, text="Trapezoid", command=is_trapezoid_check)
-# Initialized
-is_riemann_check.grid(row=0, column=3, sticky=W, pady=2)
-is_trapezoid_check.grid(row=1, column=3, sticky=W, pady=2)
-
-# Breakline
-break_line = ttk.Separator(orient=HORIZONTAL)
-break_line.grid(row=2, column=3, sticky=EW, pady=2)
-
-window.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    main_window = MainWindow(root)
+    root.mainloop()
