f444def229de6ace00b5666991e29511b6d4331b
MerinoSheep <35940408+MerinoSheep@users.noreply.github.com>
2020-07-08 22:34:01 -0500

    improved performance
    using the numexpr library to decrease time needed to calculate y values for equations

--- graph.py
+++ graph.py
@@ -6,30 +6,35 @@ from math import log10 as log  # pylint: disable=unused-import
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
-
+import numexpr as ne
+import time
 
 def draw(start_range: float, end_range: float, N: int, fx: str, drawtype: bool, RSUMTYPE: str, gui: object):
     """Starts graphing equation with sum"""
-    end_range += .1
+
     rect_linewidth = 1
-    x_vals = np.arange(float(start_range), end_range, .001)
+    x_vals = np.arange(float(start_range), end_range+.0005, .001)#.001
     arr_length = x_vals.size
-    y_val_equation = np.zeros(arr_length)
+
+    #y_val_equation = np.zeros(arr_length)
+    #y_val_equation = []
     def equation(x):  # pylint: disable=unused-argument
         return eval(fx)
 
-    delta_x = (end_range-.1)-start_range
+    delta_x = (end_range)-start_range
     rectangle_length = delta_x/N
 
-    def is_in_bounds(gui):
-        for i in range(arr_length): # pylint: disable=unused-variable  # stores the y values corresponding to the array of x values
-            x = x_vals[i]
-            try:
-                y_val_equation[i]=eval(fx)
-            except (NameError, SyntaxError):
-                gui.error_var.set("Function does not exist in range")
-                return False
+    def is_in_bounds(gui) -> bool:
+        global y_val_equation
+        x = x_vals
+        try:
+            y_val_equation = ne.evaluate(fx)
+        except (NameError, SyntaxError):
+            gui.error_var.set("Function does not exist in range")
+            return False
         return True
+
+
     if is_in_bounds(gui):
         fig, ax = plt.subplots(nrows=1, ncols=1)
         del fig
@@ -37,36 +42,36 @@ def draw(start_range: float, end_range: float, N: int, fx: str, drawtype: bool, 
         # Draws Rectangles
         if drawtype is not None and drawtype:
             if RSUMTYPE == "Left":  # LEFT RIEMANN SUMS
-                for i in np.arange(start_range, end_range-.1, rectangle_length):
+                for i in np.arange(start_range, end_range, rectangle_length):
                     current_axis.add_patch(patches.Rectangle(
                         (i, 0), (rectangle_length), equation(i),
                         linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
             elif RSUMTYPE == "Right":  # RIGHT RIEMANN SUMS
-                for i in np.arange(end_range-.1, start_range, -(rectangle_length)):
+                for i in np.arange(end_range, start_range, -(rectangle_length)):
                     current_axis.add_patch(patches.Rectangle(
                         (i-(rectangle_length), 0), (rectangle_length), equation(i),
                         linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
             elif RSUMTYPE == "Middle":  # MIDDLE RIEMANN SUMS
-                for i in np.arange(start_range+rectangle_length/2, end_range-.1, rectangle_length):
+                for i in np.arange(start_range+rectangle_length/2, end_range, rectangle_length):
                     current_axis.add_patch(patches.Rectangle(
                         (i-(rectangle_length)/2, 0), (rectangle_length), equation(i),
                         linewidth=rect_linewidth, edgecolor='r', facecolor='none'))
 
         elif drawtype is not None and not drawtype:  # TRAPEZOID
-            for i in np.arange(start_range, end_range-.1, (rectangle_length)):
+            for i in np.arange(start_range, end_range, (rectangle_length)):
                 # BL,BR,TR, TL order of how nump draws the polygons
                 x = [i, i+rectangle_length, i+rectangle_length, i, ]
                 y = [0, 0, equation(i+rectangle_length), equation(i)]
                 poly = patches.Polygon(xy=list(
                     zip(x, y)), fill=False, linewidth=rect_linewidth, edgecolor='r', facecolor='none')
                 current_axis.add_patch(poly)
-        #Does not graph asymptotes
+        # Does not graph asymptotes
         utol = 150.
         ltol = -150.
-        y_val_equation[y_val_equation > utol] = np.inf
-        y_val_equation[y_val_equation < ltol] = -np.inf
+        #y_val_equation[y_val_equation > utol] = np.inf
+        #y_val_equation[y_val_equation < ltol] = -np.inf
         ax.grid(linewidth='.25')
         ax.set_axisbelow(True)
         plt.plot(x_vals, y_val_equation)
